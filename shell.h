160bda8e6491c0fbbf5c440b07270c9f324d01a3
Fakile Razaq <razaqjimmie91@gmail.com>
2023-12-13 14:52:06 -0800

    Header File

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*---LIBRARIES---*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/*---MACROS---*/
+#define TOK_DELIM "\t\r\n\a\""
+
+void shell_interactive(void);
+void shell_inactive(void);
+char *read_stream(void);
+char *read_line(void);
+char **split_line(char *line);
+char *get_path(char *command);
+int exec_arg(char **args);
+#endif
